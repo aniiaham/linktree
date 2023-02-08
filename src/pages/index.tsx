cbdbc54cfaf2a4ed883816c0fd8e744246d7939b
aniiaham <aniya_axm@icloud.com>
2023-02-08 16:50:06 -0500

    fixed footer icons

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -74,18 +74,18 @@ function GithubIcon() {
   );
 }
 
-function LinkedinIcon() {
+function TelegramIcon() {
   return (
     <svg
       width="30"
       height="30"
-      viewBox="0 0 256 256"
+      viewBox="0 0 24 24"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
     >
       <path
-        d="M218.123 218.127H180.192V158.724C180.192 144.559 179.939 126.324 160.464 126.324C140.708 126.324 137.685 141.758 137.685 157.693V218.123H99.755V95.967H136.168V112.661H136.678C140.322 106.43 145.588 101.304 151.915 97.8292C158.242 94.3542 165.393 92.6604 172.606 92.928C211.051 92.928 218.139 118.216 218.139 151.114L218.123 218.127ZM56.955 79.27C44.798 79.272 34.941 69.418 34.939 57.261C34.937 45.104 44.79 35.247 56.947 35.245C69.104 35.242 78.961 45.096 78.963 57.253C78.9641 63.091 76.646 68.6904 72.5187 72.8194C68.3915 76.9483 62.7931 79.2687 56.955 79.27ZM75.921 218.128H37.95V95.967H75.92V218.127L75.921 218.128ZM237.033 0.0180063H18.89C8.58002 -0.0979937 0.125023 8.16101 -0.000976562 18.471V237.524C0.121023 247.839 8.57502 256.106 18.889 255.998H237.033C247.369 256.126 255.856 247.859 255.999 237.524V18.454C255.852 8.12401 247.364 -0.133994 237.033 0.00100628"
-        fill="#0A66C2"
+        d="M12 2C6.48 2 2 6.48 2 12C2 17.52 6.48 22 12 22C17.52 22 22 17.52 22 12C22 6.48 17.52 2 12 2ZM16.64 8.8C16.49 10.38 15.84 14.22 15.51 15.99C15.37 16.74 15.09 16.99 14.83 17.02C14.25 17.07 13.81 16.64 13.25 16.27C12.37 15.69 11.87 15.33 11.02 14.77C10.03 14.12 10.67 13.76 11.24 13.18C11.39 13.03 13.95 10.7 14 10.49C14.0069 10.4582 14.006 10.4252 13.9973 10.3938C13.9886 10.3624 13.9724 10.3337 13.95 10.31C13.89 10.26 13.81 10.28 13.74 10.29C13.65 10.31 12.25 11.24 9.52 13.08C9.12 13.35 8.76 13.49 8.44 13.48C8.08 13.47 7.4 13.28 6.89 13.11C6.26 12.91 5.77 12.8 5.81 12.45C5.83 12.27 6.08 12.09 6.55 11.9C9.47 10.63 11.41 9.79 12.38 9.39C15.16 8.23 15.73 8.03 16.11 8.03C16.19 8.03 16.38 8.05 16.5 8.15C16.6 8.23 16.63 8.34 16.64 8.42C16.63 8.48 16.65 8.66 16.64 8.8Z"
+        fill="black"
       />
     </svg>
   );
@@ -111,10 +111,13 @@ function LinkCard({
             <Image
               className="rounded-sm"
               alt={title}
-              src={image.includes("logos_linkedin-icon.svg")
-              ? linkedinImage
-              : image.includes("ant-design_github-filled.svg") ? gitHubImage : instagramImage
-            }
+              src={
+                image.includes("logos_linkedin-icon.svg")
+                  ? linkedinImage
+                  : image.includes("ant-design_github-filled.svg")
+                  ? gitHubImage
+                  : instagramImage
+              }
               width={40}
               height={40}
             />
@@ -143,17 +146,17 @@ export default function Home() {
         <LinkCard key={link.href} {...link} />
       ))}
       <div className="flex items-center gap-4 mt-8">
-      {data.socials.map((link) => {
-        if (link.href.includes("linkedin")) {
-          return <LinkedinIcon />;
-        }
-        if (link.href.includes("github")) {
-          return <GithubIcon />;
-        }
-        if (link.href.includes("instagram")) {
-          return <InstagramIcon />;
-        }
-      })}
+        {data.socials.map((link) => {
+          if (link.href.includes("t.me")) {
+            return <TelegramIcon />;
+          }
+          if (link.href.includes("github")) {
+            return <GithubIcon />;
+          }
+          if (link.href.includes("instagram")) {
+            return <InstagramIcon />;
+          }
+        })}
       </div>
     </div>
   );
